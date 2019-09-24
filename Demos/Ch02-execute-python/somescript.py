6a1c084e106c312f90e624ce03f2a8b84cbbddbb
Judy Lipinski <judy@karmoxie.com>
2019-09-24 08:44:06 -0700

    fixed typo, missing )

--- Demos/Ch02-execute-python/somescript.py
+++ Demos/Ch02-execute-python/somescript.py
@@ -1,10 +1,10 @@
 print(111/11)
 print(6/8)
 
-print(4/5
+print(4/5)
 
 # in a script print is needed 
-# the following doesn't do anything
+# the following doesn't do anything in script
 122/12 
 
 print (111111/11)
